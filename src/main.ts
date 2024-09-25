04cd11753bb4b0721da78fa2a8f8e6a3874410f7
Alija Bobija <bobija.alija@gmail.com>
2024-09-25 12:15:29 +0200

    Revert "Remove boilerplate code"
    This reverts commit f3d5ebf6b9017e832a3efca4d0de8c7ed88c972f.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-import './style.scss';
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-Alpine.start();
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div x-data="{ open: false }">
-    <button @click="open = ! open">Expand</button>
- 
-    <span x-show="open" x-transition>
-        Content...
-    </span>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
   </div>
-`;
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
