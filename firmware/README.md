a289170bdedb1eacb85d3e66e2b5332d13ff0a4a
Alija Bobija <bobija.alija@gmail.com>
2024-09-28 20:47:46 +0200

    connect to wifi

---
+++ firmware/README.md
@@ -0,0 +1,5 @@
+```bash
+touch sdkconfig.defaults.local
+echo "CONFIG_EXAMPLE_WIFI_SSID=\"YOUR_WIFI_SSID_HERE\"" >> sdkconfig.defaults.local
+echo "CONFIG_EXAMPLE_WIFI_PASSWORD=\"YOUR_WIFI_PASSWORD_HERE\"" >> sdkconfig.defaults.local
+```
--- firmware/main/nfcity.c
+++ firmware/main/nfcity.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
 #include "cbor.h"
 
 void app_main()
 {
-    //
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
 }
