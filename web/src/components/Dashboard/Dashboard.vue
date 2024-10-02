8410e1f5033362052d3a3a356c18b3014dbbf101
Alija Bobija <bobija.alija@gmail.com>
2024-10-02 09:36:44 +0200

    introduced info panel

--- web/src/components/Dashboard/Dashboard.vue
+++ web/src/components/Dashboard/Dashboard.vue
@@ -4,6 +4,7 @@ import Memory from '@/components/Memory/Memory.vue';
 import { hex } from '@/helpers';
 import MifareClassic from '@/models/MifareClassic';
 import { PiccType } from '@/models/Picc';
+import InfoPanel from '../InfoPanel/InfoPanel.vue';
 
 defineProps<{
   picc: MifareClassic;
@@ -33,7 +34,12 @@ defineProps<{
     </div>
 
     <div class="main">
-      <Memory :picc="picc" />
+      <div class="section">
+        <Memory :picc="picc" />
+      </div>
+      <div class="section">
+        <InfoPanel />
+      </div>
     </div>
   </div>
 </template>
---
+++ web/src/components/InfoPanel/InfoPanel.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import '@/components/InfoPanel/InfoPanel.scss';
+</script>
+
+<template>
+  <div class="info-panel">
+    Click on one of the sectors on the left to load its data.
+  </div>
+</template>
