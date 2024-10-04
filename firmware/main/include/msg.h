02e40388f1dda2181c3931c19d91710d94ca1fb6
Alija Bobija <bobija.alija@gmail.com>
2024-10-04 08:13:57 +0200

    merge enc/dec into msg

--- firmware/main/include/msg.h
+++ firmware/main/include/msg.h
@@ -2,6 +2,8 @@
 #include "cbor.h"
 #include "picc/rc522_mifare.h"
 
+// {{ encoding
+
 #define ENC_HELLO_MSG_KIND              "hello"
 #define ENC_HELLO_BYTES                 32
 #define ENC_PICC_MSG_KIND               "picc"
@@ -13,6 +15,44 @@
 #define ENC_PICC_SECTOR_MSG_KIND        "picc_sector"
 #define ENC_PICC_SECTOR_BYTES           4 * ENC_PICC_BLOCK_BYTES // FIXME: for mifare 4k
 
+// }} encoding
+
+// {{ decoding
+
+#define DEC_MSG_ID_SIZE          36 // uuid
+#define DEC_MSG_KIND_SIZE        32
+#define DEC_GET_PICC_MSG_KIND    "get_picc"
+#define DEC_READ_BLOCK_MSG_KIND  "read_block"
+#define DEC_READ_SECTOR_MSG_KIND "read_sector"
+
+typedef struct
+{
+    char id[DEC_MSG_ID_SIZE + 1];
+    char kind[DEC_MSG_KIND_SIZE + 1];
+} dec_msg_desc_t;
+
+typedef struct
+{
+    uint8_t value[RC522_MIFARE_BLOCK_SIZE];
+    rc522_mifare_key_type_t type;
+} dec_picc_key_t;
+
+typedef struct
+{
+    uint8_t address; // Block address
+    dec_picc_key_t key;
+} dec_read_block_msg_t;
+
+typedef struct
+{
+    uint8_t offset;
+    dec_picc_key_t key;
+} dec_read_sector_msg_t;
+
+// }} decoding
+
+// {{ encoding
+
 CborError enc_hello_message(CborEncoder *encoder);
 
 CborError enc_picc_message(CborEncoder *encoder, rc522_picc_t *picc);
@@ -23,3 +63,15 @@ CborError enc_picc_block_message(CborEncoder *encoder, uint8_t address, uint8_t 
 
 CborError enc_picc_sector_message(
     CborEncoder *encoder, uint8_t sector_offset, uint8_t sector_block_0_address, uint8_t *sector_data);
+
+// }} encoding
+
+// {{ decoding
+
+CborError dec_msg_desc(const uint8_t *buffer, size_t buffer_size, dec_msg_desc_t *out_msg_desc);
+
+CborError dec_read_block(const uint8_t *buffer, size_t buffer_size, dec_read_block_msg_t *out_read_block_msg);
+
+CborError dec_read_sector(const uint8_t *buffer, size_t buffer_size, dec_read_sector_msg_t *out_read_sector_msg);
+
+// }} decoding
--- firmware/main/src/msg.c
+++ firmware/main/src/msg.c
@@ -1,4 +1,6 @@
-#include "enc.h"
+#include "msg.h"
+
+// {{ encoding
 
 // TODO: Check for return values everywhere
 
@@ -125,3 +127,148 @@ CborError enc_picc_sector_message(
 
     return CborNoError;
 }
+
+// }} encoding
+
+// {{ decoding
+
+// TODO: Check for return values everywhere
+
+static CborError cbor_value_get_uint8(CborValue *value, uint8_t *result)
+{
+    uint64_t _result;
+    CborError err = cbor_value_get_uint64(value, &_result);
+
+    if (err != CborNoError) {
+        return err;
+    }
+
+    if (_result > UINT8_MAX) {
+        return CborErrorDataTooLarge;
+    }
+
+    *result = (uint8_t)_result;
+
+    return CborNoError;
+}
+
+CborError dec_msg_desc(const uint8_t *buffer, size_t buffer_size, dec_msg_desc_t *out_msg_desc)
+{
+    dec_msg_desc_t msg = { 0 };
+
+    CborError err = CborNoError;
+    CborParser parser;
+    CborValue it;
+    if ((err = cbor_parser_init(buffer, buffer_size, 0, &parser, &it)) != CborNoError) {
+        return err;
+    }
+
+    CborValue value;
+    if ((err = cbor_value_map_find_value(&it, "$id", &value)) != CborNoError) {
+        return err;
+    }
+
+    if (!cbor_value_is_text_string(&value)) {
+        return CborErrorIllegalType;
+    }
+
+    size_t len = 0;
+    if ((err = cbor_value_get_string_length(&value, &len)) != CborNoError) {
+        return err;
+    }
+
+    if (len > sizeof(msg.id) - 1) {
+        return CborErrorOverlongEncoding;
+    }
+
+    if ((err = cbor_value_copy_text_string(&value, msg.id, &len, NULL)) != CborNoError) {
+        return err;
+    }
+
+    if ((err = cbor_value_map_find_value(&it, "$kind", &value)) != CborNoError) {
+        return err;
+    }
+
+    if (!cbor_value_is_text_string(&value)) {
+        return CborErrorIllegalType;
+    }
+
+    if ((err = cbor_value_get_string_length(&value, &len)) != CborNoError) {
+        return err;
+    }
+
+    if (len > sizeof(msg.kind) - 1) {
+        return CborErrorOverlongEncoding;
+    }
+
+    if ((err = cbor_value_copy_text_string(&value, msg.kind, &len, NULL)) != CborNoError) {
+        return err;
+    }
+
+    memcpy(out_msg_desc, &msg, sizeof(msg));
+
+    return CborNoError;
+}
+
+static CborError dec_key(const CborValue *key_map, dec_picc_key_t *out_key)
+{
+    CborValue value;
+    cbor_value_map_find_value(key_map, "value", &value);
+
+    size_t len = 0;
+    cbor_value_get_string_length(&value, &len);
+
+    if (len != RC522_MIFARE_KEY_SIZE) {
+        return CborErrorUnknownLength;
+    }
+
+    dec_picc_key_t key = { 0 };
+
+    cbor_value_copy_byte_string(&value, key.value, &len, NULL);
+    cbor_value_map_find_value(key_map, "type", &value);
+    cbor_value_get_uint8(&value, (uint8_t *)&key.type);
+
+    memcpy(out_key, &key, sizeof(dec_picc_key_t));
+
+    return CborNoError;
+}
+
+CborError dec_read_block(const uint8_t *buffer, size_t buffer_size, dec_read_block_msg_t *out_read_block_msg)
+{
+    CborParser parser;
+    CborValue it;
+    cbor_parser_init(buffer, buffer_size, 0, &parser, &it);
+
+    dec_read_block_msg_t read_block_msg = { 0 };
+
+    CborValue value;
+    cbor_value_map_find_value(&it, "address", &value);
+    cbor_value_get_uint8(&value, &read_block_msg.address);
+    cbor_value_map_find_value(&it, "key", &value);
+    dec_key(&value, &read_block_msg.key);
+
+    memcpy(out_read_block_msg, &read_block_msg, sizeof(dec_read_block_msg_t));
+
+    return CborNoError;
+}
+
+CborError dec_read_sector(const uint8_t *buffer, size_t buffer_size, dec_read_sector_msg_t *out_read_sector_msg)
+{
+    CborParser parser;
+    CborValue it;
+    cbor_parser_init(buffer, buffer_size, 0, &parser, &it);
+
+    dec_read_block_msg_t read_sector_msg = { 0 };
+
+    CborValue value;
+    cbor_value_map_find_value(&it, "offset", &value);
+    cbor_value_get_uint8(&value, &read_sector_msg.address);
+    cbor_value_map_find_value(&it, "key", &value);
+    dec_key(&value, &read_sector_msg.key);
+
+    memcpy(out_read_sector_msg, &read_sector_msg, sizeof(dec_read_sector_msg_t));
+
+    return CborNoError;
+}
+
+// }} decoding
