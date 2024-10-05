de858a0cbe0b49d4dc5df6efb5c55efe33454640
Alija Bobija <bobija.alija@gmail.com>
2024-10-05 09:41:04 +0200

    show root topic in front of ping

--- web/src/components/SystemInfo/SystemInfo.vue
+++ web/src/components/SystemInfo/SystemInfo.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import Client from '@/comm/Client';
 import { onClientPing, onClientPong, onClientPongMissed } from '@/comm/hooks/ClientEventHooks';
 import '@/components/MemoryBlock/MemoryBlock.scss';
 import '@/components/SystemInfo/SystemInfo.scss';
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
 
 enum PingState {
   Undefined,
@@ -10,6 +11,8 @@ enum PingState {
   Ponged,
   PongMissed,
 }
+
+const client = inject('client') as Client;
 
 const pingState = ref<PingState>(PingState.Undefined);
 const pingLatency = ref<number | undefined>(undefined);
@@ -32,26 +35,29 @@ onClientPongMissed(() => {
 <template>
   <div class="component system-info">
     <div class="ping">
-      <span class="latency" v-if="pingLatency" title="Latency [ms]">
-        {{ pingLatency }}
-      </span>
-      <span class="miss" v-if="pingState == PingState.PongMissed">
-        miss
+      <span class="root-topic" title="mqtt root topic">
+        {{ client.rootTopic }}
       </span>
       <Transition mode="out-in" :duration="100">
-        <span class="status undefined" v-if="pingState == PingState.Undefined" title="Ping pong">
+        <span class="status undefined" v-if="pingState == PingState.Undefined" title="ping pong">
           &squf;
         </span>
-        <span class="status pinged" v-else-if="pingState == PingState.Pinged" title="Ping sent">
+        <span class="status pinged" v-else-if="pingState == PingState.Pinged" title="ping sent">
           &squf;
         </span>
-        <span class="status ponged" v-else-if="pingState == PingState.Ponged" title="Pong received">
+        <span class="status ponged" v-else-if="pingState == PingState.Ponged" title="pong received">
           &squf;
         </span>
-        <span class="status miss" v-else-if="pingState == PingState.PongMissed" title="Pong miss">
+        <span class="status miss" v-else-if="pingState == PingState.PongMissed" title="pong miss">
           &squf;
         </span>
       </Transition>
+      <span class="miss" v-if="pingState == PingState.PongMissed">
+        miss
+      </span>
+      <span class="latency" v-if="pingLatency" title="latency [ms]">
+        {{ pingLatency }}
+      </span>
     </div>
   </div>
 </template>
