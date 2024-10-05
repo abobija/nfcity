850d6275293ff0929b91c3a5c6a37656578a203a
Alija Bobija <bobija.alija@gmail.com>
2024-10-05 18:37:05 +0200

    simplified with computed()

--- web/src/components/Dashboard/TargetByteRenderer.vue
+++ web/src/components/Dashboard/TargetByteRenderer.vue
@@ -3,11 +3,15 @@ import '@/components/Dashboard/TargetByteRenderer.scss';
 import '@/components/MemoryBlock/MemoryBlock.scss';
 import { bin, hex } from '@/helpers';
 import { MifareClassicBlockGroupType, MifareClassicBlockType } from '@/models/MifareClassic';
+import { computed } from 'vue';
 import TargetByte from './TargetByte';
 
-defineProps<{
+const props = defineProps<{
   byte: TargetByte;
 }>();
+
+const group = computed(() => props.byte.group);
+const block = computed(() => props.byte.group.block);
 </script>
 
 <template>
@@ -17,19 +21,19 @@ defineProps<{
         <span class="name">byte</span>
         <span class="value" title="index">[{{ byte.index }}]</span>
 
-        <ul v-if="byte.group.block.type != MifareClassicBlockType.Undefined">
+        <ul v-if="block.type != MifareClassicBlockType.Undefined">
           <li class="item">
             <span class="name">value</span>
-            <span class="value">0x{{ hex(byte.group.block.data[byte.index]) }}</span>
+            <span class="value">0x{{ hex(block.data[byte.index]) }}</span>
 
             <ul>
               <li class="item">
                 <span class="name">dec</span>
-                <span class="value">{{ byte.group.block.data[byte.index] }}</span>
+                <span class="value">{{ block.data[byte.index] }}</span>
               </li>
               <li class="item">
                 <span class="name">bin</span>
-                <span class="value">{{ bin(byte.group.block.data[byte.index], '_')
+                <span class="value">{{ bin(block.data[byte.index], '_')
                   }}</span>
               </li>
             </ul>
@@ -38,41 +42,41 @@ defineProps<{
       </li>
       <li class="item">
         <span class="name">block</span>
-        <span class="value">{{ byte.group.block.address }}</span>
+        <span class="value">{{ block.address }}</span>
         <span class="name">sector</span>
-        <span class="value">{{ byte.group.block.sector.offset }}</span>
+        <span class="value">{{ block.sector.offset }}</span>
 
         <ul>
-          <li class="item" v-if="byte.group.block.type != MifareClassicBlockType.Undefined">
+          <li class="item" v-if="block.type != MifareClassicBlockType.Undefined">
             <span class="name">type</span>
-            <span class="value">{{ MifareClassicBlockType[byte.group.block.type] }}</span>
+            <span class="value">{{ MifareClassicBlockType[block.type] }}</span>
           </li>
           <li class="item">
             <span class="name">address</span>
-            <span class="value">0x{{ hex(byte.group.block.address) }}</span>
+            <span class="value">0x{{ hex(block.address) }}</span>
             <span class="name">offset</span>
-            <span class="value">{{ byte.group.block.offset }}</span>
+            <span class="value">{{ block.offset }}</span>
           </li>
           <li class="item">
             <span class="name">access bits</span>
             <span class="value" title="c1 c2 c3">
-              {{ byte.group.block.accessBits.c1 }}
-              {{ byte.group.block.accessBits.c2 }}
-              {{ byte.group.block.accessBits.c3 }}
+              {{ block.accessBits.c1 }}
+              {{ block.accessBits.c2 }}
+              {{ block.accessBits.c3 }}
             </span>
           </li>
         </ul>
       </li>
       <li class="item">
         <span class="name">byte group</span>
-        <span class="value">{{ MifareClassicBlockGroupType[byte.group.type] }}</span>
+        <span class="value">{{ MifareClassicBlockGroupType[group.type] }}</span>
 
         <ul>
           <li class="item">
             <span class="name">offset</span>
-            <span class="value">{{ byte.group.offset }}</span>
+            <span class="value">{{ group.offset }}</span>
             <span class="name">length</span>
-            <span class="value">{{ byte.group.length }}</span>
+            <span class="value">{{ group.length }}</span>
           </li>
         </ul>
       </li>
