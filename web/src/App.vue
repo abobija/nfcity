12473d96e43cadcfcdadf2526e03fa1665c8f1af
Alija Bobija <bobija.alija@gmail.com>
2024-10-05 08:10:54 +0200

    Handling dashboard state in dashboard

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,144 +3,42 @@ import '@/App.scss';
 import Client from '@/comm/Client';
 import Dashboard from '@/components/Dashboard/Dashboard.vue';
 import { logger } from '@/Logger';
-import MifareClassic from '@/models/MifareClassic';
-import { PiccState, PiccType } from '@/models/Picc';
 import { inject, ref, watch } from 'vue';
-import { onClientDisconnect, onClientMessage, onClientReady } from './comm/hooks/ClientEventHooks';
-import HelloDevMessage from './comm/msgs/dev/HelloDevMessage';
-import PiccDevMessage from './comm/msgs/dev/PiccDevMessage';
-import PiccStateChangedDevMessage from './comm/msgs/dev/PiccStateChangedDevMessage';
-import GetPiccWebMessage from './comm/msgs/web/GetPiccWebMessage';
+import { onClientReady } from './comm/hooks/ClientEventHooks';
 
 enum AppState {
-  Disconnected = -1,
   Initialized = 0,
   Connecting,
   Connected,
-  PiccFetching,
-  PiccNotPresent,
-  PiccRemoved,
-  PiccPaired,
 }
 
 const client = inject('client') as Client;
-const stateRef = ref<AppState>(AppState.Initialized);
-const piccRef = ref<MifareClassic | null>(null);
+const state = ref<AppState>(AppState.Initialized);
 
-watch(stateRef, (newState, oldState) => {
+watch(state, (newState, oldState) => {
   logger.verbose('app state changed from', AppState[oldState], 'to', AppState[newState]);
-
-  switch (newState) {
-    case AppState.Disconnected:
-    case AppState.Initialized:
-    case AppState.Connecting: {
-      piccRef.value = null;
-    } break;
-    case AppState.Connected: {
-      stateRef.value = AppState.PiccFetching;
-    } break;
-    case AppState.PiccFetching: {
-      client.send(GetPiccWebMessage.create());
-    } break;
-    case AppState.PiccNotPresent: {
-      // do nothing, wait for user to put a card
-    } break;
-    case AppState.PiccRemoved: {
-      // do nothing, wait for user to put the card back
-    } break;
-    case AppState.PiccPaired: {
-      // do nothing, user is in dashboard
-    } break;
-  }
 });
 
 function connect() {
-  stateRef.value = AppState.Connecting;
+  state.value = AppState.Connecting;
   client.connect();
 }
 
-onClientReady(() => stateRef.value = AppState.Connected);
-
-onClientDisconnect(() => stateRef.value = AppState.Disconnected);
-
-onClientMessage(e => {
-  if (!HelloDevMessage.is(e.message)) {
-    return;
-  }
-
-  if (stateRef.value < AppState.PiccFetching) {
-    stateRef.value = AppState.PiccFetching;
-  }
-});
-
-onClientMessage(e => {
-  const { message } = e;
-
-  if (!PiccDevMessage.is(message) && !PiccStateChangedDevMessage.is(message)) {
-    return;
-  }
-
-  const { picc } = message;
-
-  if (picc.type === PiccType.Undefined) {
-    // ignore, device did not scanned a single picc yet
-    return;
-  }
-
-  let supported = MifareClassic.dtoIsMifareClassic(picc);
-
-  // TODO: Remove once other mifare classic cards are supported
-  supported &&= picc.type === PiccType.Mifare1K;
-
-  if (!supported) {
-    logger.error('Unsupported PICC type', PiccType[picc.type]);
-    client.disconnect();
-    return;
-  }
-
-  piccRef.value = MifareClassic.fromDto(picc);
-
-  if ([PiccState.Active, PiccState.ActiveH].includes(picc.state)) {
-    stateRef.value = AppState.PiccPaired;
-    return;
-  }
-
-  if (picc.state == PiccState.Idle) {
-    if (PiccStateChangedDevMessage.is(message)
-      && [PiccState.Active, PiccState.ActiveH].includes(message.old_state)) {
-      stateRef.value = AppState.PiccRemoved;
-      return;
-    }
-
-    stateRef.value = AppState.PiccNotPresent;
-    return;
-  }
-});
+onClientReady(() => state.value = AppState.Connected);
 </script>
 
 <template>
   <div class="app">
-    <div class="enter center-screen" v-if="stateRef < AppState.Connected">
+    <div class="enter center-screen" v-if="state < AppState.Connected">
       <h1 class="title">nfcity</h1>
       <h2 class="subtitle">deep dive into NFC cards</h2>
-      <button class="connect btn primary" @click="connect" :disabled="stateRef == AppState.Connecting">
+      <button class="connect btn primary" @click="connect" :disabled="state == AppState.Connecting">
         connect
       </button>
       <div class="credits">
         made by <a href="https://github.com/abobija" target="_blank">ab</a>
       </div>
     </div>
-    <div class="picc-waiter center-screen" v-else-if="stateRef < AppState.PiccPaired">
-      <div v-if="stateRef == AppState.PiccFetching">
-        <p class="message">checking for a card...</p>
-      </div>
-      <div v-else-if="stateRef == AppState.PiccNotPresent">
-        <p class="message">put a card on the reader</p>
-      </div>
-      <div v-else-if="stateRef == AppState.PiccRemoved">
-        <p class="message">card removed, put it back please</p>
-      </div>
-    </div>
-    <Dashboard :picc="piccRef" v-else-if="piccRef instanceof MifareClassic" />
+    <Dashboard v-else />
   </div>
 </template>
--- web/src/components/Dashboard/Dashboard.vue
+++ web/src/components/Dashboard/Dashboard.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import Client from '@/comm/Client';
 import { onClientMessage } from '@/comm/hooks/ClientEventHooks';
+import HelloDevMessage from '@/comm/msgs/dev/HelloDevMessage';
+import PiccDevMessage from '@/comm/msgs/dev/PiccDevMessage';
 import PiccSectorDevMessage from '@/comm/msgs/dev/PiccSectorDevMessage';
+import PiccStateChangedDevMessage from '@/comm/msgs/dev/PiccStateChangedDevMessage';
+import GetPiccWebMessage from '@/comm/msgs/web/GetPiccWebMessage';
 import ReadSectorWebMessage from '@/comm/msgs/web/ReadSectorWebMessage';
 import '@/components/Dashboard/Dashboard.scss';
 import Memory from '@/components/Memory/Memory.vue';
@@ -17,19 +21,103 @@ import MifareClassic, {
   defaultKey,
   MifareClassicBlock,
   MifareClassicBlockByteGroupType,
-  MifareClassicBlockType
+  MifareClassicBlockType,
+  MifareClassicMemory
 } from '@/models/MifareClassic';
-import { PiccType } from '@/models/Picc';
-import { inject, ref } from 'vue';
+import { PiccState, PiccType } from '@/models/Picc';
+import { inject, onMounted, ref, watch } from 'vue';
 import SystemInfo from '../SystemInfo/SystemInfo.vue';
 
-const props = defineProps<{
-  picc: MifareClassic;
-}>();
+enum DashboardState {
+  Initialized = 0,
+  PiccFetching,
+  PiccNotPresent,
+  PiccRemoved,
+  PiccPaired,
+}
 
 const client = inject('client') as Client;
+const state = ref<DashboardState>(DashboardState.Initialized);
+const picc = ref<MifareClassic | undefined>(undefined);
+
 const targetByte = ref<MemoryByteEvent | undefined>(undefined); // Hovered byte reference
 const isTargetByteLocked = ref<boolean>(false);
+
+watch(state, (newState, oldState) => {
+  logger.debug(
+    'dashboard state changed',
+    'from', DashboardState[oldState],
+    'to', DashboardState[newState]
+  );
+
+  if (newState == DashboardState.PiccFetching) {
+    client.send(GetPiccWebMessage.create());
+  }
+});
+
+onMounted(() => {
+  state.value = DashboardState.PiccFetching;
+});
+
+onClientMessage(e => {
+  if (!HelloDevMessage.is(e.message)) {
+    return;
+  }
+
+  state.value = DashboardState.PiccFetching;
+});
+
+onClientMessage(e => {
+  const { message } = e;
+
+  if (!PiccDevMessage.is(message) && !PiccStateChangedDevMessage.is(message)) {
+    return;
+  }
+
+  const piccDto = message.picc;
+
+  if (piccDto.type === PiccType.Undefined) {
+    // ignore, device did not scanned a single picc yet
+    return;
+  }
+
+  let supported = MifareClassic.dtoIsMifareClassic(piccDto);
+
+  // TODO: Remove once other mifare classic cards are supported
+  supported &&= piccDto.type === PiccType.Mifare1K;
+
+  if (!supported) {
+    logger.error('Unsupported PICC type', PiccType[piccDto.type]);
+    client.disconnect();
+    return;
+  }
+
+  picc.value = MifareClassic.fromDto(piccDto);
+
+  if ([PiccState.Active, PiccState.ActiveH].includes(picc.value.state)) {
+    state.value = DashboardState.PiccPaired;
+    return;
+  }
+
+  if (picc.value.state == PiccState.Idle) {
+    if (PiccStateChangedDevMessage.is(message)
+      && [PiccState.Active, PiccState.ActiveH].includes(message.old_state)) {
+      state.value = DashboardState.PiccRemoved;
+      return;
+    }
+
+    state.value = DashboardState.PiccNotPresent;
+    return;
+  }
+});
+
+onClientMessage(e => {
+  if (!PiccSectorDevMessage.is(e.message)) {
+    return;
+  }
+
+  picc.value?.memory.updateSector(e.message.blocks);
+});
 
 onMemoryByteMouseEnter(e => {
   logger.verbose('Block byte entered', e);
@@ -80,129 +168,138 @@ onMemoryByteMouseClick(e => {
   isTargetByteLocked.value = true;
   targetByte.value?.focus();
 });
-
-onClientMessage(e => {
-  if (!PiccSectorDevMessage.is(e.message)) {
-    return;
-  }
-
-  props.picc.memory.updateSector(e.message.blocks);
-});
 </script>
 
 <template>
   <div class="dashboard component" :class="{ 'target-byte-locked': isTargetByteLocked }">
-    <div class="header">
-      <div class="picc">
-        <div class="general">
-          <h1 class="type">{{ PiccType[picc.type] }}</h1>
-          <ul class="metas">
-            <li class="meta">
-              <span class="name">UID</span>
-              <span class="value">{{ hex(picc.uid) }}</span>
-            </li>
-            <li class="meta">
-              <span class="name">ATQA</span>
-              <span class="value">{{ hex(picc.atqa) }}</span>
-            </li>
-            <li class="meta">
-              <span class="name">SAK</span>
-              <span class="value">{{ hex(picc.sak) }}</span>
-            </li>
-          </ul>
-        </div>
-        <div class="memory txt-nowrap">
-          {{ picc.memory.blockDistribution.flatMap(d => `${d[0]} sectors with ${d[1]} blocks`).join(' & ') }}
-          >> {{ MifareClassicBlock.size }} bytes per block
-          >> {{ picc.memory.size }} bytes of memory
-        </div>
+    <div class="scene picc-waiter center-screen" v-if="state < DashboardState.PiccPaired">
+
+      <div v-if="state == DashboardState.PiccFetching">
+        <p class="message">checking for a card...</p>
+      </div>
+      <div v-else-if="state == DashboardState.PiccNotPresent">
+        <p class="message">put a card on the reader</p>
+      </div>
+      <div v-else-if="state == DashboardState.PiccRemoved">
+        <p class="message">card removed, put it back please</p>
       </div>
 
-      <SystemInfo />
     </div>
+    <div class="scene main" v-else-if="state == DashboardState.PiccPaired && picc">
 
-    <div class="main">
-      <div class="section">
-        <Memory :memory="picc.memory" />
-      </div>
-      <div class="section">
-        <div class="info-panel">
-          <Transition appear>
-            <p class="hint" v-if="picc.memory.isEmpty">
-              Click on one of the sectors on the left to load its data.
-            </p>
-          </Transition>
-
-          <div class="target" v-if="targetByte">
-            <ul>
-              <li class="item">
-                <span class="name">byte</span>
-                <span class="value">[{{ targetByte.byteIndex }}]</span>
-
-                <ul v-if="targetByte.block.type != MifareClassicBlockType.Undefined">
-                  <li class="item">
-                    <span class="name">value</span>
-                    <span class="value">0x{{ hex(targetByte.block.data[targetByte.byteIndex]) }}</span>
-
-                    <ul>
-                      <li class="item">
-                        <span class="name">dec</span>
-                        <span class="value">{{ targetByte.block.data[targetByte.byteIndex] }}</span>
-                      </li>
-                      <li class="item">
-                        <span class="name">bin</span>
-                        <span class="value">{{ bin(targetByte.block.data[targetByte.byteIndex], '_')
-                          }}</span>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
+      <div class="header">
+        <div class="picc">
+          <div class="general">
+            <h1 class="type">{{ PiccType[picc.type] }}</h1>
+            <ul class="metas">
+              <li class="meta">
+                <span class="name">UID</span>
+                <span class="value">{{ hex(picc.uid) }}</span>
               </li>
-              <li class="item">
-                <span class="name">block</span>
-                <span class="value">{{ targetByte.block.address }}</span>
-                <span class="name">sector</span>
-                <span class="value">{{ targetByte.block.sector.offset }}</span>
-
-                <ul>
-                  <li class="item" v-if="targetByte.block.type != MifareClassicBlockType.Undefined">
-                    <span class="name">type</span>
-                    <span class="value">{{ MifareClassicBlockType[targetByte.block.type] }}</span>
-                  </li>
-                  <li class="item">
-                    <span class="name">address</span>
-                    <span class="value">0x{{ hex(targetByte.block.address) }}</span>
-                    <span class="name">offset</span>
-                    <span class="value">{{ targetByte.block.offset }}</span>
-                  </li>
-                  <li class="item">
-                    <span class="name">access bits</span>
-                    <span class="value">
-                      {{ targetByte.block.accessBits.c1 }}
-                      {{ targetByte.block.accessBits.c2 }}
-                      {{ targetByte.block.accessBits.c3 }}
-                    </span>
-                    <span class="name">(c1 c2 c3)</span>
-                  </li>
-                </ul>
+              <li class="meta">
+                <span class="name">ATQA</span>
+                <span class="value">{{ hex(picc.atqa) }}</span>
               </li>
-              <li class="item">
-                <span class="name">byte group</span>
-                <span class="value">{{ MifareClassicBlockByteGroupType[targetByte.byteGroup.type] }}</span>
-
-                <ul>
-                  <li class="item">
-                    <span class="name">offset</span>
-                    <span class="value">{{ targetByte.byteGroup.offset }}</span>
-                    <span class="name">length</span>
-                    <span class="value">{{ targetByte.byteGroup.length }}</span>
-                  </li>
-                </ul>
+              <li class="meta">
+                <span class="name">SAK</span>
+                <span class="value">{{ hex(picc.sak) }}</span>
               </li>
             </ul>
           </div>
+          <div class="memory txt-nowrap">
+            {{ picc.memory.blockDistribution.flatMap(d => `${d[0]} sectors with ${d[1]} blocks`).join(' & ') }}
+            >> {{ MifareClassicBlock.size }} bytes per block
+            >> {{ picc.memory.size }} bytes of memory
+          </div>
+        </div>
+
+        <SystemInfo />
+      </div>
+
+      <div class="main">
+        <div class="section">
+          <Memory :memory="picc.memory as MifareClassicMemory" />
+        </div>
+        <div class="section">
+          <div class="info-panel">
+            <Transition appear>
+              <p class="hint" v-if="picc.memory.isEmpty">
+                Click on one of the sectors on the left to load its data.
+              </p>
+            </Transition>
+
+            <div class="target" v-if="targetByte">
+              <ul>
+                <li class="item">
+                  <span class="name">byte</span>
+                  <span class="value">[{{ targetByte.byteIndex }}]</span>
+
+                  <ul v-if="targetByte.block.type != MifareClassicBlockType.Undefined">
+                    <li class="item">
+                      <span class="name">value</span>
+                      <span class="value">0x{{ hex(targetByte.block.data[targetByte.byteIndex]) }}</span>
+
+                      <ul>
+                        <li class="item">
+                          <span class="name">dec</span>
+                          <span class="value">{{ targetByte.block.data[targetByte.byteIndex] }}</span>
+                        </li>
+                        <li class="item">
+                          <span class="name">bin</span>
+                          <span class="value">{{ bin(targetByte.block.data[targetByte.byteIndex], '_')
+                            }}</span>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li class="item">
+                  <span class="name">block</span>
+                  <span class="value">{{ targetByte.block.address }}</span>
+                  <span class="name">sector</span>
+                  <span class="value">{{ targetByte.block.sector.offset }}</span>
+
+                  <ul>
+                    <li class="item" v-if="targetByte.block.type != MifareClassicBlockType.Undefined">
+                      <span class="name">type</span>
+                      <span class="value">{{ MifareClassicBlockType[targetByte.block.type] }}</span>
+                    </li>
+                    <li class="item">
+                      <span class="name">address</span>
+                      <span class="value">0x{{ hex(targetByte.block.address) }}</span>
+                      <span class="name">offset</span>
+                      <span class="value">{{ targetByte.block.offset }}</span>
+                    </li>
+                    <li class="item">
+                      <span class="name">access bits</span>
+                      <span class="value">
+                        {{ targetByte.block.accessBits.c1 }}
+                        {{ targetByte.block.accessBits.c2 }}
+                        {{ targetByte.block.accessBits.c3 }}
+                      </span>
+                      <span class="name">(c1 c2 c3)</span>
+                    </li>
+                  </ul>
+                </li>
+                <li class="item">
+                  <span class="name">byte group</span>
+                  <span class="value">{{ MifareClassicBlockByteGroupType[targetByte.byteGroup.type] }}</span>
+
+                  <ul>
+                    <li class="item">
+                      <span class="name">offset</span>
+                      <span class="value">{{ targetByte.byteGroup.offset }}</span>
+                      <span class="name">length</span>
+                      <span class="value">{{ targetByte.byteGroup.length }}</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
+
     </div>
   </div>
 </template>
