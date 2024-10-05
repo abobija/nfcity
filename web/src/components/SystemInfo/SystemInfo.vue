aaca7fc679fb7026e92f7ebce8a3f0308bc1e1e1
Alija Bobija <bobija.alija@gmail.com>
2024-10-05 08:23:00 +0200

    fixed ponged-miss sysinfo state

--- web/src/components/SystemInfo/SystemInfo.vue
+++ web/src/components/SystemInfo/SystemInfo.vue
@@ -4,47 +4,51 @@ import '@/components/MemoryBlock/MemoryBlock.scss';
 import '@/components/SystemInfo/SystemInfo.scss';
 import { ref } from 'vue';
 
-enum PingPongState {
+enum PingState {
   Undefined,
   Pinged,
   Ponged,
   PongMissed,
 }
 
-const pingPongState = ref<PingPongState>(PingPongState.Undefined);
-const pingPongLatency = ref<number | undefined>(undefined);
+const pingState = ref<PingState>(PingState.Undefined);
+const pingLatency = ref<number | undefined>(undefined);
 
-onClientPing(() => pingPongState.value = PingPongState.Pinged);
+onClientPing(() => {
+  if (pingState.value != PingState.PongMissed) {
+    pingState.value = PingState.Pinged;
+  }
+});
 onClientPong((e) => {
-  pingPongState.value = PingPongState.Ponged;
-  pingPongLatency.value = e.latency;
+  pingState.value = PingState.Ponged;
+  pingLatency.value = e.latency;
 });
 onClientPongMissed(() => {
-  pingPongState.value = PingPongState.PongMissed;
-  pingPongLatency.value = undefined;
+  pingState.value = PingState.PongMissed;
+  pingLatency.value = undefined;
 });
 </script>
 
 <template>
   <div class="component system-info">
     <div class="ping">
-      <span class="latency" v-if="pingPongLatency" title="Latency [ms]">
-        {{ pingPongLatency }}
+      <span class="latency" v-if="pingLatency" title="Latency [ms]">
+        {{ pingLatency }}
       </span>
-      <span class="miss" v-if="pingPongState == PingPongState.PongMissed">
+      <span class="miss" v-if="pingState == PingState.PongMissed">
         miss
       </span>
       <Transition mode="out-in" :duration="100">
-        <span class="status undefined" v-if="pingPongState == PingPongState.Undefined" title="Ping pong">
+        <span class="status undefined" v-if="pingState == PingState.Undefined" title="Ping pong">
           &squf;
         </span>
-        <span class="status pinged" v-else-if="pingPongState == PingPongState.Pinged" title="Ping sent">
+        <span class="status pinged" v-else-if="pingState == PingState.Pinged" title="Ping sent">
           &squf;
         </span>
-        <span class="status ponged" v-else-if="pingPongState == PingPongState.Ponged" title="Pong received">
+        <span class="status ponged" v-else-if="pingState == PingState.Ponged" title="Pong received">
           &squf;
         </span>
-        <span class="status miss" v-else-if="pingPongState == PingPongState.PongMissed" title="Pong missed">
+        <span class="status miss" v-else-if="pingState == PingState.PongMissed" title="Pong missed">
           &squf;
         </span>
       </Transition>
