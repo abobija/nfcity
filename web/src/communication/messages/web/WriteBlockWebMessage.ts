b497b30aee224277e06388bcb6d17ba856da2241
Alija Bobija <bobija.alija@gmail.com>
2024-10-12 09:24:09 +0200

    write_block msg implements picc_dto

--- web/src/communication/messages/web/WriteBlockWebMessage.ts
+++ web/src/communication/messages/web/WriteBlockWebMessage.ts
@@ -2,13 +2,15 @@ import PiccBlockDto from "@/communication/dtos/PiccBlockDto";
 import { AuthorizedWebMessage, WebMessageKind } from "@/communication/Message";
 import { PiccKey } from "@/models/Picc";
 
-export default class WriteBlockWebMessage extends AuthorizedWebMessage {
+export default class WriteBlockWebMessage extends AuthorizedWebMessage implements PiccBlockDto {
   readonly $kind: WebMessageKind = 'write_block';
-  readonly block: PiccBlockDto;
+  readonly address: number;
+  readonly data: Uint8Array;
 
   protected constructor(block: PiccBlockDto, key: PiccKey) {
     super(key);
-    this.block = block;
+    this.address = block.address;
+    this.data = block.data;
   }
 
   static from(block: PiccBlockDto, key: PiccKey): WriteBlockWebMessage {
