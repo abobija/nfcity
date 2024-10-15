9d3bd313fec3a5369ce8b406d777b06847ad47f2
Alija Bobija <bobija.alija@gmail.com>
2024-10-15 20:04:02 +0200

    console's buildSectorTrailer fn

--- web/src/console.ts
+++ web/src/console.ts
@@ -1,5 +1,12 @@
 import WriteBlockWebMessage from "@/communication/messages/web/WriteBlockWebMessage";
-import { accessBitsComboPoolToBytes, defaultKey } from "@/models/MifareClassic";
+import {
+  AccessBitsComboPool,
+  accessBitsComboPoolToBitsPool,
+  accessBitsComboPoolToBytes,
+  accessBitsPoolToBytes,
+  blockSize,
+  defaultKey
+} from "@/models/MifareClassic";
 import { KeyType } from "@/models/Picc";
 import Client from "./communication/Client";
 import { assert, bin, hex, unhexToArray } from "./utils/helpers";
@@ -17,6 +24,7 @@ class Console {
       hex,
       bin,
       accessBitsComboPoolToBytes,
+      unhexToArray,
     };
 
     for (const [key, value] of Object.entries(functionsToExpose)) {
@@ -26,19 +34,27 @@ class Console {
     }
   }
 
+  get client() {
+    assert(this._client);
+    return this._client;
+  }
+
   set client(client: Client) {
     this._client = client;
   }
 
-  async piccWrite(
+  async writeBlock(
     address: number,
     data: string,
     key: string = hex(defaultKey.value),
     keyType: KeyType = defaultKey.type,
   ) {
-    assert(this._client);
+    assert(typeof address === 'number');
+    assert(typeof data === 'string');
+    assert(typeof key === 'string');
+    assert(typeof keyType === 'number');
 
-    return await this._client.send(
+    return await this.client.send(
       new WriteBlockWebMessage(
         address,
         Uint8Array.from(unhexToArray(data)),
@@ -48,6 +64,31 @@ class Console {
         }
       )
     )
+  }
+
+  buildSectorTrailer(
+    keyA: string,
+    accessBitsComboPool: AccessBitsComboPool,
+    userByte: string,
+    keyB: string,
+  ): number[] {
+    assert(typeof keyA === 'string');
+    assert(typeof accessBitsComboPool === 'object');
+    assert(typeof userByte === 'string');
+    assert(typeof keyB === 'string');
+
+    const accessBitsPool = accessBitsComboPoolToBitsPool(accessBitsComboPool);
+
+    const data = [
+      ...unhexToArray(keyA),
+      ...accessBitsPoolToBytes(accessBitsPool),
+      ...unhexToArray(userByte),
+      ...unhexToArray(keyB),
+    ];
+
+    assert(data.length === blockSize);
+
+    return data;
   }
 }
 
